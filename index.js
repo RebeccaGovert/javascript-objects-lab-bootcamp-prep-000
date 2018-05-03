14e331e9a52e738bac176bae5607010a24439dfe
Rebecca Govert <becci.govert@gmail.com>
2018-05-03 16:02:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var recipes = { ingredients: 'measurment' };
 
-function updateObjectWithKeyAndValue(object, key, value) {
+function updateObjectWithKeyAndValue(object, key, value) { //non destructive updates object
   return Object.assign(object, { [key]: value });
 }
 
-function updateObjectWithKeyAndValue (object, key, value) {
+function updateObjectWithKeyAndValue (object, key, value) { // non destructive gives new object with update
  var newObj = Object.assign({}, object, { [key]: value });
  return newObj;
 }
+
 
 
